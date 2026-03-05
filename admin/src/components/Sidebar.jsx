eed7f1959bc10bb27189b00929c5d87fff32f0e5
Mario Marquestó <mariomarquesto@gmail.com>
2026-03-04 21:12:50 -0300

    cambio de color el sidear

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -3,8 +3,8 @@ import { NAVIGATION } from "./Navbar";
 
 function Sidebar() {
   return (
-    <aside className="w-64 min-h-full bg-base-100 border-r border-base-300 p-4">
-      <ul className="menu space-y-2">
+<aside className="w-64 min-h-screen bg-gray-900 text-white p-4">     
+   <ul className="menu space-y-2">
         {NAVIGATION.map((item) => (
           <li key={item.name}>
             <NavLink to={item.path}>
